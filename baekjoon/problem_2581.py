580c0bc918599c14f1bf4d5f5e21341b69030c05
김경빈 <beenkim@beenkim-MacBookPro.local>
2022-04-07 13:05:59 +0900

    [BOJ] 소수

--- baekjoon/problem_2581.py
+++ baekjoon/problem_2581.py
@@ -5,7 +5,7 @@ prime = []
 
 for i in range(M, N+1):
     for j in range(2, i+1):
-        if i==j:
+        if i == j:
             prime.append(i)
         if i%j == 0:
             break
@@ -15,4 +15,3 @@ if not prime:
 else:
     print(sum(prime))
     print(min(prime))
-    
