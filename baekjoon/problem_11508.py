d9fa329cd2fd99be3af3c326d4b09e9fbb6f77a6
김경빈 <beenkim@beenkim-MacBookPro.local>
2022-04-07 13:01:35 +0900

    [BOJ] 2+1 세일

---
+++ baekjoon/problem_11508.py
@@ -0,0 +1,21 @@
+def getMinPrice(N, price):
+    total = 0
+    idx = 0
+
+    for _ in range(N//3):
+        total += sum(price[idx:idx+2])
+        idx += 3
+
+    if N%3 != 0:
+        total += sum(price[idx:len(price)])
+
+    return total
+
+
+N = int(input())
+price = [ int(input()) for _ in range(N) ]
+
+price = sorted(price, reverse=True)
+res = sum(price) if N <= 2 else getMinPrice(N, price)
+
+print(res)
