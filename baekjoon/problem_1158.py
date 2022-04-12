063d33dabfc15190b1e14ba5de45fd7cf0983db1
BeenKimKr <bin9610@gmail.com>
2022-04-12 14:10:01 +0900

    [BOJ] 요세푸스 문제

--- baekjoon/problem_1158.py
+++ baekjoon/problem_1158.py
@@ -5,6 +5,8 @@ circle = list(range(1, n + 1))
 answer = []
 idx = 0
 
+# k번째 index만 가져옴
+# index가 리스트를 초과하는 경우 => index를 그 초과분으로 초기화
 while circle:
     idx = (idx + k - 1) % len(circle)
     answer.append(str(circle.pop(idx)))
