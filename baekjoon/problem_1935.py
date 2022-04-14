d9aaef1c2a8476dc3a85ba212b8849bbb83ff7c6
BeenKimKr <bin9610@gmail.com>
2022-04-15 02:45:15 +0900

    [BOJ] 후위 표기식2

--- baekjoon/problem_1935.py
+++ baekjoon/problem_1935.py
@@ -8,16 +8,16 @@ for i in range(n):
     numDict[chr(65 + i)] = sys.stdin.readline().strip()
 
 operator = '+-*/'
-numQueue = deque(list())
+numStack = deque(list())
 
 for i in postfix:
     if i in numDict.keys():
-        numQueue.append(numDict[i])
+        numStack.append(numDict[i])
     elif i in operator:
-        if len(numQueue) >= 2:
-            y = numQueue.pop()
-            x = numQueue.pop()
-            numQueue.append(str(eval(x + i + y)))
+        if len(numStack) >= 2:
+            y = numStack.pop()
+            x = numStack.pop()
+            numStack.append(str(eval(x + i + y)))
 
-answer = "%0.2f" % float(numQueue[0])
+answer = "%0.2f" % float(numStack[0])
 print(answer)
