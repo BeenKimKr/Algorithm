ab8a6fa8769d5b9692d86fa365c2bfb00b8a365f
BeenKimKr <bin9610@gmail.com>
2022-04-14 14:47:36 +0900

    [BOJ] 프린터 큐

---
+++ baekjoon/problem_1966.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+d = int(input())
+
+for _ in range(d):
+    n, m = map(int, sys.stdin.readline().split())
+    queue = deque(list(map(int, sys.stdin.readline().strip().split())))
+    target = queue[m]
+    cnt = 1
+
+    while queue:
+        first = max(queue)
+        item = queue.popleft()
+        m -= 1
+
+        if len(queue) == 0:
+            print(cnt)
+            break
+        elif item == first:
+            if item == target and m == -1:
+                print(cnt)
+                break
+            cnt += 1
+        else:
+            queue.append(item)
+            m = len(queue) - 1 if m == -1 else m
