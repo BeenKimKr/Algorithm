fed2f03c2d00f27c64132ce46986fe31c13611cc
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-30 23:13:23 +0900

    [BOJ] 수 정렬하기 2

---
+++ etc/problem_10816.py
@@ -0,0 +1,21 @@
+n = int(input())
+lst_n = input().split()
+m = int(input())
+lst_m = input().split()
+res_dict = dict()
+res_str = ''
+
+
+for i in lst_n:
+    if i in res_dict.keys():
+        res_dict[i] += 1
+    else:
+        res_dict[i] = 1
+
+for i in lst_m:
+    if i in res_dict.keys():
+        res_str += (str(res_dict[i]) + ' ')
+    else:
+        res_str += '0 '
+
+print(res_str)
