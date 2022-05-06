d6f724fccf7785884c5acf697719570f927c7715
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-05-06 21:36:53 +0900

    [BOJ] 수 정렬하기 3

--- etc/problem_10989.py
+++ etc/problem_10989.py
@@ -2,10 +2,11 @@ import sys
 
 N = int(input())
 nums = [0 for _ in range(10001)]
-for i in range(N):
+idx = 0
+
+for _ in range(N):
     nums[int(sys.stdin.readline().strip())] += 1
 
-idx = 0
 for i in nums:
     if i != 0:
         for _ in range(i):
