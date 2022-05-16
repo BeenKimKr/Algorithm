eee71dd7196a3ae0d8d320933ffd9a56da73ba35
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-05-16 18:05:15 +0900

    [BOJ] 2+1 세일

--- baekjoon/problem_11508.py
+++ baekjoon/problem_11508.py
@@ -6,13 +6,14 @@ def getMinPrice(N, price):
         total += sum(price[idx:idx+2])
         idx += 3
 
-    if N%3 != 0:
-        total += sum(price[idx:len(price)])
+    if N % 3 != 0:
+        total += sum(price[idx:])
 
     return total
 
+
 N = int(input())
-price = [ int(input()) for _ in range(N) ]
+price = [int(input()) for _ in range(N)]
 
 price = sorted(price, reverse=True)
 res = sum(price) if N <= 2 else getMinPrice(N, price)
