91a17c8045fd8189ccc9ea404f82b524bbd28b36
BeenKimKr <bin9610@gmail.com>
2022-06-07 00:08:19 +0900

    [BOJ]  A+B - 8

--- etc/problem_11022.py
+++ etc/problem_11022.py
@@ -3,5 +3,5 @@ input = sys.stdin.readline
 
 n = int(input())
 for i in range(1, n+1):
-    x, y = map(int, input().split())
-    print('Case #', i, ':', x, '+', y, '=', x+y)
+    x, y = input().split()
+    print('Case #' + str(i) + ': ' + x + ' + ' + y + ' =', int(x)+int(y))
